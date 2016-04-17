3e09600603f6959595431416061f5924720b9bb6
cdolm92 <cdolm92@gmail.com>
2016-04-17 00:15:53 -0400

    imported Social framework to add SLComposeViewController for Twitter and Facebook and an UIActivityViewController

--- Project3/Project1/DetailViewController.swift
+++ Project3/Project1/DetailViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2016 Christella. All rights reserved.
 //
 
+import Social
 import UIKit
 
 class DetailViewController: UIViewController {
@@ -32,8 +33,10 @@ class DetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         self.configureView()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Action, target: self, action:#selector(DetailViewController.shareTapped))
+        
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -44,6 +47,30 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+    
+    func shareTapped() {
+//        let vc = UIActivityViewController(activityItems: [detailImageView.image!], applicationActivities:[])
+//        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+//        presentViewController(vc, animated: true, completion: nil)
+        
+        
+        //FACEBOOK
+        
+//        let vc = SLComposeViewController(forServiceType: SLServiceTypeFacebook)
+//        vc.setInitialText("Look at this great picture!")
+//        vc.addImage(detailImageView.image!)
+//        vc.addURL(NSURL(string: "http://www.photolib.noaa.gov/nssl"))
+//        presentViewController(vc, animated: true, completion: nil)
+        
+        //TWITTER
+        
+        let vc = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+        vc.setInitialText("Look at this great picture!")
+        vc.addImage(detailImageView.image!)
+        vc.addURL(NSURL(string: "http://www.photolib.noaa.gov/nssl"))
+        presentViewController(vc, animated: true, completion: nil)
+        
     }
 
     override func didReceiveMemoryWarning() {
